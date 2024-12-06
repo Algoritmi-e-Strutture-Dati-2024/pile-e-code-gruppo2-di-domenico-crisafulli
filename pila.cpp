bb27461ac724082299fb617da66e833c3cd3f146
simonedido <s.didomenico1@studenti.uniba.it>
2024-12-06 11:22:25 +0100

    Update pila.cpp

--- pila.cpp
+++ pila.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "ListaPuntatori.cpp"
+#include "ListaPuntatori.cpp" 
 #include <stdexcept>
 
 template <typename T>
@@ -9,33 +9,37 @@ private:
 
 public:
     // Aggiunge un elemento alla pila
-    void push(T elemento) {
-        lista.inslista(elemento, 1); 
+    void push(T elemento, int *testa) {
+
+        lista.inslista(elemento, lista[testa]); 
+        ++(*testa);
     }
 
     // Rimuove l'elemento in cima alla pila e lo restituisce
-    T pop() {
+    T pop(int *testa) {
         if (isEmpty()) {
             cout << "Errore: pila vuota, impossibile eseguire pop." << endl;
         }
         T topElem = lista.leggilista(1);
         lista.canclista(1); // Rimuove l'elemento in testa
+        *testa--;
         return topElem;
+
     }
 
     // Restituisce l'elemento in cima alla pila senza rimuoverlo
     T top() {
-      return lista.leggilista(1);
+      
+        return lista.leggilista(1); 
+    }
+    // Verifica se la pila è vuota
+    bool isEmpty() {
+        return lista.listavuota();
     }
 
-
-    //Restituisce true se la pila è vuota, altrimenti false
-    bool isEmpty(){
-      return lista.listavuota();
+    // Restituisce il numero di elementi nella pila
+    int size() {
+        return lista.lunghezza();
     }
-
-    //Restituisce il numero di elementi nella pila
-    int size(){
-      return lista.lunghezza();
-    }
+    
 };
